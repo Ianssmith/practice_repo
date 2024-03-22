43eb0bad9436e917ac8c1e16d44483d6cb50085e
ianssmith <ian.s.smith0317@gmail.com>
2024-03-21 22:08:00 -0400

    adds query and results for question2 challenge1, restructures proj directories

--- challenge1/queries1.sql
+++ challenge1/queries1.sql
@@ -1,15 +1,28 @@
-use challenge1;
+USE challenge1;
 
 -- 1. What is the total amount each customer spent at the restaurant?
-select sales.customer_id, sum(price) from menu 
-left join sales 
-on menu.product_id = sales.product_id
-group by sales.customer_id
+SELECT sales.customer_id, SUM(price) FROM menu 
+LEFT JOIN sales 
+ON menu.product_id = sales.product_id
+GROUP BY sales.customer_id
 ; 
 
 -- 2. How many days has each customer visited the restaurant?
+SELECT sales.customer_id, COUNT(DISTINCT order_date) FROM sales
+GROUP BY sales.customer_id
+;
 
 -- 3. What was the first item from the menu purchased by each customer?
+-- select sales.product_id
+SELECT menu.product_name
+FROM( SELECT ROW_NUMBER() OVER(PARTITION BY sales.customer_id ORDER BY sales.order_date ASC) AS orders
+    FROM sales
+    INNER JOIN menu
+    ON sales.product_id = menu.product_id) AS order_list
+LEFT JOIN sales
+ON menu.product_id = sales.product_id
+WHERE orders = 1 
+;
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 
