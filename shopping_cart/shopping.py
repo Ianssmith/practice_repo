f46e571ab0e87a5e71d952012bf6d5b5e1860dad
ianssmith <ian.s.smith0317@gmail.com>
2024-03-26 16:09:04 -0400

    adds more methods; and beginning cart data and functions

--- shopping_cart/shopping.py
+++ shopping_cart/shopping.py
@@ -1,15 +1,19 @@
-data = [{'srno':1,
+data = [{'srno':'1',
          'name':'Chicken',
          'quant':700,
          'cost':9},
-         {'srno':2,
+         {'srno':'2',
          'name':'Eggs',
          'quant':4200,
          'cost':3.50},
-         {'srno':3,
+         {'srno':'3',
          'name':'Bread',
          'quant':24,
          'cost':4}]
+
+shop_addresss = '234 234 st city, state, 12345'
+shipping = 0
+cart_total = 0
 
 def print_menu():
     print('Sr.no     Item         Quantity     Cost/Item \n ------  ------      -----------     ----------')
@@ -18,6 +22,11 @@ def print_menu():
 
 def get_item_request():
     item = input('What would you like to purchase?')
+    quant = get_quantity_request(item)
+    if item == '1':
+    elif item == '2':
+    elif item == '3':
+    else:
     return item
 
 def get_quantity_request(item):
@@ -39,12 +48,23 @@ customers = [{'id':'1',
               'lastorder':'01-01-2023'
             }]
 
+def get_distance(address):
+    pass
+
 def get_cust_info(id):
     for i in customers:
         if i['id'] == id:
             return i
 
-def calc_shipping():
+def calc_shipping(id):
+    for i in customers:
+        if i['id'] == id:
+            #distance = get_distance(i['address'])
+            distance = 20 #placeholder value
+            if distance >= 15 and distance <=30:
+                return 100
+            elif distance <15:
+                return 50
     #<15km = 50
     #>=15<=30km = 100
     pass
@@ -53,6 +73,10 @@ def display_bill_info():
     pass
 
 def dist_conversion(val,to_mi=True):
+    if to_mi:
+        return val/1.609344
+    else:
+        return val*1.690344
     pass
 
 def return_or_quit():
@@ -64,23 +88,39 @@ def return_or_quit():
     else:
         print('invalid menu option, please restart the program.')
 
+def display_cart():
+    print(f'items     quantities\n ========     ====\n{cart['items']} {cart['quantities']}')
+
+cart = {"items":[],
+        "quantities":[]}
 
 
 def main():
     print_menu()
-    selection = input('What would you like to do:\n press 1 to display customer info\n press 2 to update your info\n press 3 to place a new order\n press 4 to checkout and display your info')
+    id = input('Welcome to magic store fun arena.. Enter your customer id')
+    shipping = calc_shipping(id)
+
+    selection = input('What would you like to do:\n press 1 to display customer info\n press 2 to update your info\n press 3 to place a new order\n press 4 to checkout and display your info\n press 5 to display cart')
     if selection == '1':
-        id = input('enter your customer id')
+        #display customer info
         customerinfo = get_cust_info(id)
         print(customerinfo)
         return_or_quit()
     elif selection =='2':
-        pass
+        #update customer info
+        customerinfo = get_cust_info(id)
+        print(customerinfo)
+        return_or_quit()
     elif selection =='3':
+        #place a new order
         citem = get_item_request()
-        cquant = get_quantity_request(citem)
+        return_or_quit()
     elif selection =='4':
-        pass
+        #checkout and display customer info
+        return_or_quit()
+    elif selection =='5':
+        display_cart()
+        return_or_quit()
     else:
         print('invalid menu selection')
 
